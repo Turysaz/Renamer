2de1af63953cb74660b990f1f78ca730490278bb
Daniel Rydén <danryd@gmail.com>
2013-01-21 12:54:26 +0100

    initial

---
+++ Renamer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args == null || args[0] == "-?")
+                PrintHelp();
+            var directory = Environment.CurrentDirectory;
+            var replace = args[0];
+            var target = args[1];
+
+            if (args[0].ToLower() == "--rootpath")
+            {
+                directory = args[1];
+                replace = args[2];
+                target = args[3];
+
+            }
+          
+                Rename(directory, replace, target);
+   
+            
+
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: [--rootpath pathToSolution] stringToReplace targetString");
+            Console.WriteLine(" -? for help");
+        }
+
+        public static void Rename(string rootdir, string replace, string target)
+        {
+            var files = Directory.GetFiles(rootdir);
+            Console.WriteLine(String.Format("Found {0} files in {1}", files.Length, rootdir));
+            foreach (var file in files)
+            {
+                if (Path.GetExtension(file) != "exe" && Path.GetExtension(file) != "dll")
+                {
+                    try
+                    {
+                        var content = File.ReadAllText(file);
+                        if (content.Contains(replace))
+                        {
+                            var newContent = content.Replace(replace, target);
+                            File.WriteAllText(file, newContent);
+                        }
+                        if (Path.GetFileName(file).Contains(replace))
+                            File.Move(file, Path.Combine(Path.GetDirectoryName(file), Path.GetFileName(file).Replace(replace, target)));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(String.Format("Trouble with {0}, in {1}, issue:{2}", file, rootdir, e.Message));
+                        Environment.Exit(1);
+
+                    }
+                }
+            }
+            var dirs = Directory.GetDirectories(rootdir);
+            foreach (var dir in dirs)
+            {
+                Rename(dir, replace, target);
+                try
+                {
+                    var topFolder = dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar)).Replace(Path.DirectorySeparatorChar.ToString(),"");
+                    if (topFolder.Contains(replace))
+                    {
+                        var targetDir = dir.Substring(0, dir.LastIndexOf(Path.DirectorySeparatorChar));
+                        targetDir = Path.Combine(targetDir, topFolder.Replace(replace, target));
+                        Directory.Move(dir, targetDir);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(String.Format("Trouble with {0}, issue:{1}", dir, e.Message));
+                    Environment.Exit(1);
+
+                }
+            }
+        }
+    }
+}
