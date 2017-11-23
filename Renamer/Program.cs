af5622247a66beded7af94063a8cbc1c7a7b9a01
Turysaz <turysaz@posteo.org>
2017-11-23 10:13:55 +0100

    Cleanup

--- Renamer/Program.cs
+++ Renamer/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-
-namespace Renamer
+﻿namespace Renamer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+
     class Program
     {
         static void Main(string[] args)
@@ -14,9 +14,10 @@ namespace Renamer
             {
                 PrintHelp();
             }
-            var directory = Environment.CurrentDirectory;
-            var replace = args[0];
-            var target = args[1];
+
+            string directory = Environment.CurrentDirectory;
+            string replace = args[0];
+            string target = args[1];
 
             if (args[0].ToLower() == "--rootpath")
             {
@@ -25,68 +26,135 @@ namespace Renamer
                 target = args[3];
 
             }
-          
-                Rename(directory, replace, target);
-   
-            
 
+            RecursiveRename(directory, replace, target);
         }
+
 
         private static void PrintHelp()
         {
-            Console.WriteLine("Usage: [--rootpath pathToSolution] stringToReplace targetString");
-            Console.WriteLine(" -? for help");
+            Console.WriteLine(
+                    "Usage: [--rootpath pathToSolution] " +
+                    "stringToReplace targetString");
+
+            Console.WriteLine(
+                    " -? for help");
+
             Environment.Exit(0);
         }
 
-        public static void Rename(string rootdir, string replace, string target)
+
+        public static void RecursiveRename(
+            string rootdir,
+            string replace,
+            string target)
         {
-            var files = Directory.GetFiles(rootdir);
-            Console.WriteLine(String.Format("Found {0} files in {1}", files.Length, rootdir));
-            foreach (var file in files)
+            // Handle files
+            string[] files = Directory.GetFiles(rootdir);
+            Console.WriteLine($"Found {files.Length} files in {rootdir}");
+
+            foreach (string file in files)
             {
-                if (Path.GetExtension(file) != "exe" && Path.GetExtension(file) != "dll")
+                if (Path.GetExtension(file) != "exe"
+                        && Path.GetExtension(file) != "dll")
                 {
                     try
                     {
-                        var content = File.ReadAllText(file);
-                        if (content.Contains(replace))
-                        {
-                            var newContent = content.Replace(replace, target);
-                            File.WriteAllText(file, newContent);
-                        }
-                        if (Path.GetFileName(file).Contains(replace))
-                            File.Move(file, Path.Combine(Path.GetDirectoryName(file), Path.GetFileName(file).Replace(replace, target)));
+                        ReplaceInFile(file, replace, target);
+                        RenameFile(file, replace, target);
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine(String.Format("Trouble with {0}, in {1}, issue:{2}", file, rootdir, e.Message));
-                        Environment.Exit(1);
+                        Console.WriteLine(
+                                $"Trouble with {file} in {rootdir}, " +
+                                $"issue: {e.Message}");
 
+                        Environment.Exit(1);
                     }
                 }
             }
-            var di = new DirectoryInfo(rootdir);
-            var dirs = di.EnumerateDirectories().Where(d => !d.Name.StartsWith(".")).Select(d=>d.FullName);
-            foreach (var dir in dirs)
-            {
-                Rename(dir, replace, target);
-                try
-                {
-                    var topFolder = dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar)).Replace(Path.DirectorySeparatorChar.ToString(),"");
-                    if (topFolder.Contains(replace))
-                    {
-                        var targetDir = dir.Substring(0, dir.LastIndexOf(Path.DirectorySeparatorChar));
-                        targetDir = Path.Combine(targetDir, topFolder.Replace(replace, target));
-                        Directory.Move(dir, targetDir);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(String.Format("Trouble with {0}, issue:{1}", dir, e.Message));
-                    Environment.Exit(1);
 
+            // Recurse subdirectories
+            DirectoryInfo di = new DirectoryInfo(rootdir);
+            List<string> dirs =
+                di.EnumerateDirectories()
+                .Where(d => !d.Name.StartsWith("."))
+                .Select(d => d.FullName).ToList();
+
+            foreach (string dir in dirs)
+            {
+                RecursiveRename(dir, replace, target);
+                RenameDirectory(dir, replace, target);
+            }
+        }
+
+
+        private static void ReplaceInFile(
+                string filepath,
+                string replace,
+                string target)
+        {
+            string content = File.ReadAllText(filepath);
+
+            if (content.Contains(replace))
+            {
+                string newContent = content.Replace(replace, target);
+                File.WriteAllText(filepath, newContent);
+            }
+        }
+
+
+        private static void RenameFile(
+                string filepath,
+                string replace,
+                string target)
+        {
+            if (Path.GetFileName(filepath).Contains(replace))
+            {
+                File.Move(
+                        filepath,
+                        Path.Combine(
+                            Path.GetDirectoryName(filepath),
+                            Path.GetFileName(filepath)
+                            .Replace(replace, target)));
+            }
+        }
+
+
+        private static void RenameDirectory(
+            string dir,
+            string replace,
+            string target)
+        {
+            try
+            {
+                string topFolder =
+                    dir.Substring(
+                            dir.LastIndexOf(
+                                Path.DirectorySeparatorChar))
+                    .Replace(
+                            Path.DirectorySeparatorChar.ToString(),
+                            "");
+
+                if (topFolder.Contains(replace))
+                {
+                    string targetDir =
+                        dir.Substring(
+                                0,
+                                dir.LastIndexOf(Path.DirectorySeparatorChar));
+
+                    targetDir =
+                        Path.Combine(
+                                targetDir,
+                                topFolder.Replace(replace, target));
+
+                    Directory.Move(dir, targetDir);
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Trouble with {dir}, issue:{e.Message}");
+                Environment.Exit(1);
             }
         }
     }
